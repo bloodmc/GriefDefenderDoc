a00806227b3c1318a589e02c16fc4b35134a93bf
FITFC <101124415+FITFC@users.noreply.github.com>
2022-10-06 19:43:52 -0500

    up

--- docs/br/_nav-br.ts
+++ docs/br/_nav-br.ts
@@ -1,14 +1,14 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const en = navbar([
-  { text: "Project Home", link: "/", icon: "home" },
+  { text: "Início", link: "/", icon: "home" },
   { text: "Wiki", link: "/wiki/", icon: "edit" },
   { text: "Hooks", link: "/hooks/", icon: "plugin"},
   { text: "API", link: "/api", icon: "api" },
   { text: "Mods", link: "/mods/", icon: "toolbox" },
   { text: "FAQ", link: "/faq/", icon: "faq" },
   {
-	text: "Support",
+	text: "Suporte",
     icon: "support",
     children: [
 	  { text: "Patreon", link: "https://www.patreon.com/bloodmc" },
