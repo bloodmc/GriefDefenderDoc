0a760bed71c5887b75116842b94e7bfae94dfbad
FITFC <101124415+FITFC@users.noreply.github.com>
2022-10-09 22:23:35 -0500

    up

--- docs/br/_nav-br.ts
+++ docs/br/_nav-br.ts
@@ -1,12 +1,12 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const br = navbar([
-  { text: "Project Home", link: "/", icon: "home" },
-  { text: "Wiki", link: "/wiki/", icon: "edit" },
-  { text: "Hooks", link: "/hooks/", icon: "plugin"},
-  { text: "API", link: "/api", icon: "api" },
-  { text: "Mods", link: "/mods/", icon: "toolbox" },
-  { text: "FAQ", link: "/faq/", icon: "faq" },
+  { text: "Project Home", link: "/br/", icon: "home" },
+  { text: "Wiki", link: "/br/wiki/", icon: "edit" },
+  { text: "Hooks", link: "/br/hooks/", icon: "plugin"},
+  { text: "API", link: "/br/api", icon: "api" },
+  { text: "Mods", link: "/br/mods/", icon: "toolbox" },
+  { text: "FAQ", link: "/br/faq/", icon: "faq" },
   {
 	text: "Support",
     icon: "support",
