f116ca5d7bd4831ca3bbcff4458324d090083f9d
bloodshot <jdroque@gmail.com>
2024-06-23 17:34:35 -0400

    Make sure javascript runs after DOM content loads.

--- docs/.vuepress/styles/enhanceApp.js
+++ docs/.vuepress/styles/enhanceApp.js
@@ -1,15 +1,26 @@
-// docs/.vuepress/enhanceApp.js
-export default ({ router }) => {
-  if (typeof window !== 'undefined') {
-    document.addEventListener('DOMContentLoaded', () => {
-      const menuButton = document.querySelector('.navbar-toggle');
-      const menu = document.querySelector('.navbar-menu');
+// .vuepress/enhanceApp.js
+import { defineClientConfig } from '@vuepress/client';
 
-      if (menuButton && menu) {
-        menuButton.addEventListener('click', () => {
+export default defineClientConfig({
+  setup() {
+    if (typeof window !== 'undefined') {
+      const setupMenu = () => {
+        const menuButton = document.querySelector('.navbar-toggle');
+        const menu = document.querySelector('.navbar-menu');
+
+        const toggleMenu = () => {
           menu.classList.toggle('active');
-        });
-      }
-    });
-  }
-};
+        };
+
+        if (menuButton && menu) {
+          menuButton.addEventListener('click', toggleMenu);
+        }
+      };
+
+      document.addEventListener('DOMContentLoaded', setupMenu);
+      router.afterEach(() => {
+        setupMenu();
+      });
+    }
+  },
+});
